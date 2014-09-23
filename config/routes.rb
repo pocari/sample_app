7c588298494403ee5a8fa27ad9977d5a1718ebf5
pocari <caffelattenonsugar@gmail.com>
2014-09-23 12:42:47 +0900

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "stati_pages/home"
+  get "stati_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
