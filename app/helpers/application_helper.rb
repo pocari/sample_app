5200ff022d6d9ecb306f3077b5820bd8ed21d6c7
pocari <caffelattenonsugar@gmail.com>
2014-10-04 20:49:06 +0900

    add layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  BASE_TITLE = "Ruby on Rails Tutorial Sample App"
+  #ページ毎の完全なタイトルを返します
+  def full_title(page_title)
+    if page_title.empty?
+      BASE_TITLE
+    else
+      "#{BASE_TITLE} | #{page_title}"
+    end
+  end
 end
