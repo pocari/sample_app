568f110f37a569b5240291e6fc29ff99576e2ad9
pocari <caffelattenonsugar@gmail.com>
2014-10-05 19:51:08 +0900

    practice 6.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
     self.email.downcase!
   }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, 
                     format: {with: VALID_EMAIL_REGEX},
