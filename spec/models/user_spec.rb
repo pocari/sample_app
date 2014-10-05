f86f8c1a3f0d99b8405116d21ae3bedc85630c0a
pocari <caffelattenonsugar@gmail.com>
2014-10-05 16:51:47 +0900

    Make a basic User model(including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 
 describe User do
 
-  before {@user = User.new(name: "Example User", email: "user@example.com")}
+  before {
+    @user = User.new(
+      name: "Example User",
+      email: "user@example.com",
+      password: "foobar",
+      password_confirmation: "foobar"
+    )
+  }
 
   subject{@user}
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid}
 
@@ -21,6 +32,49 @@ describe User do
     before {@user.email = ''}
 
     it {should_not be_valid}
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+
+    it {should_not be_valid}
+  end
+
+  describe "when password_confirmation is not present" do
+    before {@user.password_confirmation = ''}
+
+    it {should_not be_valid}
+  end
+
+  describe "when password does'nt match confirmation" do
+    before {@user.password_confirmation = 'mismatch'}
+
+    it {should_not be_valid}
+  end
+
+  describe "return value of authenticate method" do
+    before {@user.save}
+    let(:found_user) {User.find_by(email: @user.email)}
+
+    describe "with valid passowrd" do
+      it {should eq found_user.authenticate(@user.password)}
+    end
+
+    describe "with invalid password" do
+      let (:user_for_invalid_password) {
+        found_user.authenticate("invalid_passowrd")
+      }
+      it { should_not eq user_for_invalid_password }
+      specify {expect(user_for_invalid_password).to be_false}
+    end
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it {should be_invalid}
   end
 
   describe "when name is too long" do
