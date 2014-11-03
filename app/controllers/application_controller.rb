68cfc2ec98e060ec40aad0c36c630062cc97e986
pocari <caffelattenonsugar@gmail.com>
2014-11-03 22:22:35 +0900

    Add user following

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  around_filter :transaction_fileter
+
+  private
+  def transaction_fileter
+    ActiveRecord::Base.transaction do
+      yield
+    end
+  end
 end
